34c65a9633a0f336cbed192a994e772a89e8d6a7
freecash123 <stakeg56@gmail.com>
2026-07-28 05:19:09 +0100

    Add models index

---
+++ backend/src/models/index.js
@@ -0,0 +1,23 @@
+const User = require('./User');
+const Transaction = require('./Transaction');
+const PaymentRequest = require('./PaymentRequest');
+const Referral = require('./Referral');
+const MembershipPlan = require('./MembershipPlan');
+const SupportTicket = require('./SupportTicket');
+const BlogPost = require('./BlogPost');
+const Announcement = require('./Announcement');
+const Notification = require('./Notification');
+const AuditLog = require('./AuditLog');
+
+module.exports = {
+  User,
+  Transaction,
+  PaymentRequest,
+  Referral,
+  MembershipPlan,
+  SupportTicket,
+  BlogPost,
+  Announcement,
+  Notification,
+  AuditLog,
+};
